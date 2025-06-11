c9bf2008647293e6d1ecc468de4a9b99e4895eb4
Christian Silva <tininjacms@gmail.com>
2025-06-11 17:57:27 -0300

    feat(MeetEntity): Criaraá o meetEntity

---
+++ MeetReservation/Models/Commands/MeetEntity.cs
@@ -0,0 +1,20 @@
+﻿using MeetReservation.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetReservation.Models.Commands
+{
+    public class MeetEntity: BaseEntity
+    {
+        [Required(ErrorMessage = "O campo responsável é obrigatório")]
+        public string Responsible { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo Sala é obrigatório")]
+        public int Room { get; set; }
+        
+        [Required(ErrorMessage = "O campo Tempo inicial é obrigatório")]
+        public DateTime StartTime { get; set; }
+        
+        [Required(ErrorMessage = "O campo Tempo final é obrigatório")]
+        public DateTime EndTime { get; set; }
+    }
+}
