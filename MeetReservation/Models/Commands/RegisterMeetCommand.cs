f9a5d4db87c8aabf9f7be7123d5f0d54abb6d54c
Christian Silva <tininjacms@gmail.com>
2025-06-11 17:48:55 -0300

    feat(RegisterMeetCommand): Criará o RegisterMeetCommand

---
+++ MeetReservation/Models/Commands/RegisterMeetCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetReservation.Models.Commands
+{
+    public class RegisterMeetCommand
+    {
+        public string Responsible { get; set; } = string.Empty;
+        public int Room { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
